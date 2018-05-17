4256aa669a334005eebdc4409ad893b3380aed3e
queenielyk <queenielyk@gmail.com>
2018-05-17 20:16:57 +0800

    Add models.Student

--- self_develop/mainsite/models.py
+++ self_develop/mainsite/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    Student_ID = models.IntegerField(default=0)
+    Name = models.CharField(max_length=200)
+    def __int__(self):
+        return self.Student_ID
