5761a71a33b71be163e0e02178153f232232de30
queenielyk <queenielyk@gmail.com>
2018-05-19 22:17:24 +0800

    modified models.py Added Department.models, Course.models, Studied.models. Superuser ID: admin

--- self_develop/mainsite/admin.py
+++ self_develop/mainsite/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Student, Department, Course, Studied
+
+admin.site.register(Student)
+admin.site.register(Department)
+admin.site.register(Course)
+admin.site.register(Studied)
