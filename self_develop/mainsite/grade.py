aa7c09f4eeff195793332ff05b7c6e68cb80a4a6
chiehhsi <j4500123@gmail.com>
2018-05-24 17:46:03 +0800

    modified grade.py .html

--- self_develop/mainsite/grade.py
+++ self_develop/mainsite/grade.py
@@ -5,7 +5,17 @@ from django.template import Context, Template
 from django.template.loader import get_template
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import auth
-from django.contrib import messages
+from django.contrib import messages 
+from .models import Student, Department, Course, Studied
+
+
 
 def grade(request):
-    return render(request,'grade.html')
+
+    myString= "this is a python string."
+    data={  'student': Student.objects.all(),
+            'department':Department.objects.all(),
+            'course':Course.objects.all(),
+            'studied':Studied.objects.all()
+           }
+    return render(request,'grade.html',{"myString": myString},data)
